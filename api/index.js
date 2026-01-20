cd104e1ed618b1898f1d09087751e20cc5b314a9
wesscheid <wesscheid@gmail.com>
2026-01-19 20:51:56 -0600

    feat: restore Vercel entry point api/index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
 import app from '../server/server.js';
 
 export default app;
+=======
+const app = require('../server/server.js');\nmodule.exports = app;
+>>>>>>> 8ae4890 (feat: restore Vercel entry point api/index.js)
